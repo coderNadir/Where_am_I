add19b9248c64137768af4821b23cf3bcd07c1eb
coderNadir <coder.nadir@gmail.com>
2021-05-31 12:30:22 +0100

    Selected the button & the country container, and added the renderData function

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////////////////
+// // ** ** renderData
+const renderData = function (data, className = "") {
+  const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  data.population / 1000000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+            </div>
+    </article>
+    `;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+};
+
+///////////////////////////////////////////////////
 // ** ** ** Promisifying the geoLocation API ** ** ** //
 
 const getPosition = function () {
